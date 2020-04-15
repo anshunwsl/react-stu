ac6303e2d51a403a1c06f176a21e6d1847f0efa2
wangsl <Admin123>
2020-04-15 09:26:01 +0800

    添加顺序查找示例代码

--- src/basic/str.js
+++ src/basic/str.js
@@ -1,3 +1,24 @@
 //主要处理字符串相关的操作。。
 
 
+let data="aaaabbbbbbbbbcaa";
+//
+let flag=data[0];
+let results=[];
+let temp=[];
+for (let i=0;i<data.length;i++){
+    //
+    if(data[i]===flag){
+        temp.push(data[i]);
+    }else{
+        //
+        flag=data[i];
+        //
+        console.log(`key is ${flag}`);
+        results.push(temp.join(""));
+        temp=[flag];
+        //res
+    }
+}
+//
+console.log(results);
